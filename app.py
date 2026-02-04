9a388f7b6991f1cf301514093b4df0bc9387a4ea
lkoffa-spec <lkoffa@terpmail.umd.edu>
2026-02-03 20:21:12 -0500

    Added more to opening text in line 8

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Le-Nair Koffa!'
+    return 'Hello World from Le-Nair Koffa! This is my first HTML page'
 
 
 if __name__ == '__main__':
